dc44da71e81b3a0d3ced7b901de6100d706c8746
akashramesh13 <akashramesh13@gmail.com>
2022-08-07 15:00:04 +0530

    remove: default expenses

--- src/app.js
+++ src/app.js
@@ -14,28 +14,6 @@ const container = document.getElementById("app");
 const root = createRoot(container);
 const store = configureStore();
 
-store.dispatch(
-  addExpense({
-    description: "Water bill",
-    amount: 4500,
-  })
-);
-
-store.dispatch(
-  addExpense({
-    description: "Gas bill",
-    amount: 1000,
-    createdAt: 1000,
-  })
-);
-
-store.dispatch(
-  addExpense({
-    description: "Rent",
-    amount: 109500,
-  })
-);
-
 store.dispatch(sortByDate());
 
 const jsx = (
